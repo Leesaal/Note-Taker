4bb34352c3d159348480d70d47abd7e8990cce16
Leesa A <leesaali7@gmail.com>
2023-07-13 20:32:57 +0100

    start server errors

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const route = require("express").Router();
 const notes = require("../db/db.json");
-const uuid = require("uuid");
+// const uuid = require("uuid");
+const fs = require("fs");
 
 //Get
 route.get("/", (req, res) => {});
+
+module.exports = route;
--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const PORT = process.env.PORT || 3001;
 const path = require("path");
 const fs = require("fs");
 const notes = require("./routes/notes");
+const api = require("./routes/api");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/", api);
+app.use("/notes", notes);
 
 //listen to port
 app.listen(PORT, () =>
