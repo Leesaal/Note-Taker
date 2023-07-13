566a5875aab499835d6cf4462a9bfa13878b1a0d
Leesa A <leesaali7@gmail.com>
2023-07-13 21:29:28 +0100

    added uuid function

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 const Router = require("express").Router();
 const notes = require("../db/db.json");
-// const uuid = require("uuid");
 const fs = require("fs");
 
 //Get
-Router.get("/", (req, res) => {});
+Router.get("/", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    try {
+      const note = JSON.parse(data);
+      return res.json(note);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
 
 module.exports = Router;
